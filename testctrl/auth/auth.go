e0d3024cbf1df27b2b4329a1a8113f937a2221c8
Ben Reed <benreed@google.com>
2020-03-09 17:03:52 -0700

    Move kubernetes authentication to top-level package

--- testctrl/auth/auth.go
+++ testctrl/auth/auth.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package kubernetes
+package auth
 
 import (
 	"k8s.io/client-go/kubernetes"
